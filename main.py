29fe35fd031d0dadcc9cba41220efc6fe3499020
qanpi <qanpii@gmail.com>
2020-09-01 23:13:48 +0300

    Modified example code

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import stega as st
 import timeit as tt
 
 #Prerequisites
-text = open("messages/test.txt").read()
-img = cv2.imread("images/img.png")
+text = open("input/example.txt").read()
+img = cv2.imread("images/lena.png")
 
 e_binary = st.encode_message(text)
 e_result = st.put_binary(img, e_binary)
@@ -14,7 +14,9 @@ e_result = st.put_binary(img, e_binary)
 d_binary = st.get_binary(e_result)
 d_result = st.decode_message(d_binary)
 
-with open("output/decoded.txt", "w") as output_txt:
+cv2.imwrite("images/lena_encoded.png", e_result)
+
+with open("output/example.txt", "w") as output_txt:
     output_txt.write(d_result)
 
 
