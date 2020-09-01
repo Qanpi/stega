28f9ea3d92989cd9784b0ea072592416b0d7d721
qanpi <qanpii@gmail.com>
2020-09-01 21:11:06 +0300

    Polished the major commit. Added some new functionality.

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from cv2 import cv2
 import numpy as np
 
-import spython
+import stega as st
 
-#Initialize prerequisites
-text = open("test.txt").read() * 6480
-img = cv2.imread("images/img.png", cv2.IMREAD_GRAYSCALE)
+#Prerequisites
+text = open("messages/test.txt").read()
+img = cv2.imread("images/img.png")
 
-if __name__ == "__main__":
-    lsb = spython.LSB(img)
 
-    lsb.generate_bits(text)
+e_binary = st.encode_message(text)
+e_result = st.put_binary(img, e_binary)
 
-    output = lsb.v_inject_bits()
-    cv2.imshow("test", output)
-    cv2.waitKey()
+d_binary = st.get_binary(e_result)
+d_result = st.decode_message(d_binary)
+
+with open("output/decoded.txt", "w") as output_txt:
+    output_txt.write(d_result)
+
+cv2.imshow("test", e_result)
+cv2.waitKey(0)
+
+
+#For review later
+# b, g, r = cv2.split(img)
+
+# bits.resize(img.shape[:2])
+# b = st.inject_bits(b, bits).astype(np.uint8)
+# g = st.inject_bits(g, bits).astype(np.uint8)
+# r = st.inject_bits(r, bits).astype(np.uint8)
+# result = cv2.merge((b, g, r))
+
+# cv2.imshow("Encoded image", result)
+# cv2.imshow("Original image", img)
+# cv2.imshow("Difference", np.subtract(result, img) * 255)
+# cv2.waitKey(0)
+
+# bits = st.scrape_bits(b)
+# msg = st.decode_message(bits)
+
+
--- stega.py
+++ stega.py
@@ -3,12 +3,16 @@ import numpy as np
 
 #ENCODING ----------------------------------------------------------------------------------------------
 
-def encode_message(msg):
-    bits = np.full(len(msg) * 8, 0)
+def encode_message(msg, size=None):
+    """Converts a text message into an array of binary data"""
+    size = size if size else len(msg) * 8
+    bits = np.full(size, 0)
 
+    #Skipping over 8 elements for better performance (maybe) haven't tested if it actually improves performance
     for i in range(0, bits.size, 8):        
         o = ord(msg[i//8])
 
+        #Get binary and embed it into the list
         binary = bin(o)[2:]
         binary = binary.zfill(8)
         bits[i:i+8] = list(binary)
@@ -20,26 +24,48 @@ def inject_bits(i, b):
 
 inject_bits = np.vectorize(inject_bits) #vectorizing for better performance
 
+def put_binary(img, binary, ci=0):
+    channel = img[:,:, ci] #ci stands for channel id aka which channel to insert binary into (first channel by default)
+    
+    if binary.shape != channel.shape:
+        binary = np.resize(binary, channel.shape) #if the binary array is not big enough, it will loop over
+
+    output = inject_bits(channel, binary)
+    img[:,:, ci] = output.astype(np.uint8) #uint8 used cuz otherwise cv2 starts complaining about some depth stuff
+    return img 
+        
+
 #DECODING ----------------------------------------------------------------------------------------------
 
-def scrape_bits(byte):
-    mask = 1
-    return byte & mask #return only a certain bit that is overlapped by the mask
+def scrape_bits(b):
+    return b & 1 #return only a the least significant bit
 
 scrape_bits = np.vectorize(scrape_bits) #vectorizing for better performance
+
+def get_binary(img, ci=0):
+    channel = img[:,:, ci]
+
+    return scrape_bits(channel)
     
-def decode_message(bits):
-    bits = bits.flatten() #flatten the array to prevent from errors due to multiple dimensions
+def decode_message(binary):
+    binary = binary.flatten() #flatten the array to prevent from errors due to multiple dimensions
 
     message = ""
-    for i in range(0, bits.size, 8):
-        byte = bits[i:i+8]
+    for i in range(0, binary.size, 8):
+        byte = binary[i:i+8]
         byte = "".join(str(b) for b in byte) #convert an array of 8 bits [0,1,1,0, ...] into a string "0110..."
         ch = chr(int(byte, 2)) #get the character represented by the binary 
 
         message += ch
-
     return message
+
+#OTHER ----------------------------------------------------------------------------------------------
+
+def difference(x, y, ci=0):
+    x = x[:,:, ci]
+    y = y[:,:, ci]
+
+    return (x - y) * 255 #multiplied by 255 to exaggerate the difference
 
 
 
