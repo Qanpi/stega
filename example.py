87d1af58fcfcd055ed54a679b34f6d03cf5aad3d
qanpi <qanpii@gmail.com>
2020-09-08 00:16:19 +0300

    Support for images as messages and multi-channel injections

--- example.py
+++ example.py
@@ -2,31 +2,31 @@ from cv2 import cv2
 import argparse
 import ntpath
 
+import numpy as np
+
 import stega as st
 
 # INITIALIZING PARSES CLASS FOR CMD LINE ARGUMENTS
 parser = argparse.ArgumentParser(description="Encode a message into an image and decode it back.")
 parser.add_argument("image",          metavar="I",  type=str, help="a path to an image")
-parser.add_argument("message",        metavar="M",  type=str, help="a path to the text file (message)")
+parser.add_argument("message",        metavar="M",  type=str, help="a path to the message file")
 parser.add_argument("-o", "--output", metavar="p",  type=str, help="a path for the output text and image (default: 'output')", default="output",)
 
 args = parser.parse_args()
-print(args)
 
 # CREATING BASIC VARIABLES ---
-img = cv2.imread(args.image)
-text = open(args.message).read()
+image = cv2.imread(args.image)
+message = st.read_message(args.message, ci=(0,3))
 
 # EXAMPLE CODE ---
-e_binary = st.msg_to_binary(text)
-e_result = st.insert_binary(img, e_binary, 2)
+e_binary = st.convert_msg_to_binary(message)
+e_result = st.insert_binary(image, e_binary, ci=(0,3))
 
-d_binary = st.extract_binary(e_result, 2)
-d_result = st.binary_to_msg(d_binary)
+d_binary = st.extract_binary(e_result, ci=(0,3))
+d_result = st.convert_binary_to_msg(d_binary)
+st.write_message(d_result, "output/" + ntpath.basename(args.message))
 
-with open(args.output + "/" + ntpath.basename(args.message), "w") as output_txt:
-    output_txt.write(d_result)
-
-cv2.imwrite(args.output + "/" + ntpath.basename(args.image), e_result)
-cv2.imshow("test", e_result)
+cv2.imshow("original", image)
+cv2.imshow("encoded", e_result)
+cv2.imshow("decoded", np.resize(d_result, (220, 220, 3)))
 cv2.waitKey(0)
--- stega.py
+++ stega.py
@@ -1,64 +1,83 @@
 import numpy as np
+from cv2 import cv2
+
+import struct
+import ntpath
 
 #ENCODING ----------------------------------------------------------------------------------------------
 
-def msg_to_binary(msg):
-    """Converts a text message into an array of binary data"""
-    binary = np.array([ord(ch) for ch in msg], dtype=np.uint8) #numpy array of arrays containing ord() of one character 8 times
-    binary = np.repeat(binary, 8)
+def read_message(path, ci=(0,1)):
+    ext = ntpath.splitext(path)[-1]
+    
+    if   ext == ".txt":         
+        file_content = open(path, mode="rb").read()        
+        message = struct.unpack("B" * len(file_content), file_content)
+    elif ext in [".png", ".jpg", ".jpeg"]: message = cv2.imread(path)[:,:, ci[0]:ci[1]]
+    else: raise TypeError("Unsupported file extension.")
 
-    iterator = np.tile(np.arange(8, dtype=np.uint8), binary.size // 8)
+    return message
+
+def convert_msg_to_binary(msg):
+    """Convert a message (array of 8-bit ints) into an array of binary data"""
+    msg = np.repeat(msg, 8)
+
+    iterator = np.tile(np.arange(8, dtype=np.uint8), msg.size // 8)
     mask = np.left_shift(1, iterator)
 
-    bit = np.bitwise_and(binary, mask)
-    output = np.right_shift(bit, iterator)
-    return output
+    bit = np.bitwise_and(msg, mask)
+    binary = np.right_shift(bit, iterator)
+    return binary
 
-def inject_bits(x1, x2):
+def _inject_bits(x1, x2):
     x1 = np.bitwise_and(x1, 254)       
     return np.bitwise_or(x1, x2)
 
-def insert_binary(img, binary, ci=0):
-    """Inserts binary data into the least significant bit of a color channel value"""
-    channel = img[:,:, ci] #ci stands for channel id aka which channel to insert binary into (first channel by default)
+def insert_binary(img, binary, ci=(0,1)):
+    """Insert binary data into the least significant bit of a color channel value"""
+    copy = np.copy(img)
+    channel = copy[:,:, ci[0]:ci[1]] #ci stands for channel id aka which channel to insert binary into (first channel by default)
     
     binary = np.resize(binary, channel.shape) #if the binary array is not big enough, it will loop over
 
-    output = inject_bits(channel, binary)
-    img[:,:, ci] = output
-    return img 
+    output = _inject_bits(channel, binary)
+    copy[:,:, ci[0]:ci[1]] = output
+    return copy 
         
 
 #DECODING ----------------------------------------------------------------------------------------------
 
-def scrape_bits(x1):
+def _scrape_bits(x1):
     return np.bitwise_and(x1, 1) #return only the least significant bit
 
-def extract_binary(img, ci=0):
-    """Extracts binary data from the least significant bit of a color channel value"""
-    channel = img[:,:, ci]
-    return scrape_bits(channel)
+def extract_binary(img, ci=(0,1)):
+    """Extract binary data from the least significant bit of a color channel value"""
+    channel = img[:,:, ci[0]:ci[1]]
+    return _scrape_bits(channel)
 
-def binary_to_msg(binary):
-    """Converts an array of binary data into a text message"""
+def convert_binary_to_msg(binary):
+    """Convert an array of binary data into a message"""
     groups = binary.size // 8
-    binary = np.reshape(binary, (groups, 8))
+    binary = np.resize(binary, (groups, 8))
 
     iterator = np.tile(np.arange(8, dtype=np.uint8), (groups, 1))
 
     binary = np.left_shift(binary, iterator)
-    unic = np.bitwise_or.reduce(binary, axis=1)
+    message = np.bitwise_or.reduce(binary, axis=1)
 
-    output = [chr(c) for c in unic]
-    message = "".join(output)
     return message
+
+def write_message(msg, path):
+    ext = ntpath.splitext(path)[-1]
+    if ext == ".txt": open(path, "wb").write(struct.pack("B" * msg.size, *msg.flatten()))
+    elif ext in [".png", ".jpeg", ".jpg"]: cv2.imwrite(path, msg)
 
 #OTHER ----------------------------------------------------------------------------------------------
 
-def difference(x, y, ci=0):
-    x = x[:,:, ci]
-    y = y[:,:, ci]
-    return (x - y) * 255 #multiplied by 255 to exaggerate the difference
+def difference(x, y, ci=(0,1)):
+    a = x[:,:, ci[0]:ci[1]]
+    b = y[:,:, ci[0]:ci[1]]
+    print(a-b)
+    return (a - b) 
 
 
 
